15a2c8c8559b37a5e6197cffe92ce3ebc88c5c3e
azer elesgerli <145683767+AzerElesgerli@users.noreply.github.com>
2023-11-29 10:01:59 +0400

    task

---
+++ Areas/Pronia/ViewModels/Product/UpdateProductVM.cs
@@ -0,0 +1,20 @@
+﻿using ProniaWebApplication.Models;
+
+namespace ProniaWebApplication.Areas.Pronia.ViewModels.Product
+{
+	public class UpdateProductVM
+	{
+		public string Title { get; set; }
+
+
+		public double Price { get; set; }
+
+		public string Image { get; set; }
+
+
+		public int? CategoryId { get; set; }
+		public List<int> TagIds { get; set; }
+		public List<Category>? Categories { get; set; }
+		public List<int>? Tags { get; }
+	}
+}
